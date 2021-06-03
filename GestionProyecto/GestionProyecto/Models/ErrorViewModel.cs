37f5d9fc9350d72317e14243ee9695b08b79e260
Glorenzana <dmayena@gmail.com>
2021-06-02 23:11:37 -0600

    Proyecto

---
+++ GestionProyecto/GestionProyecto/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GestionProyecto.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
