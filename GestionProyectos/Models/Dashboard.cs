aa0424ef5d89c4986248ff3f557b5c7a8d41e848
dmelaragt83 <85272430+dmelaragt83@users.noreply.github.com>
2021-06-05 00:46:41 -0600

    Add files via upload
    instalación de imágenes y objetos

---
+++ GestionProyectos/Models/Dashboard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestionProyectos.Models
+{
+    public class Dashboard
+    {
+        public List<PROYECTO> PROYECTO { get; set; }
+        public List<REQUERIMIENTO> REQUERIMIENTO  { get; set; }
+        public List<TAREA> TAREA { get; set; }
+    }
+}
